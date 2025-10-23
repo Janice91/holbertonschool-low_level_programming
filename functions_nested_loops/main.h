03b139986bcd92180fdeca1668b22822bbc4b116
Janice91 <Kouakou.janice.2002@gmail.com>
2025-10-23 11:03:11 +0000

    Ajout du prototype print_alphabet_x10 dans main.h

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -4,11 +4,12 @@
 /**
  * File: main.h
  * Author: Janice91
- * Description: En-tête contenant les prototypes des fonctions
- * utilisées dans le projet functions_nested_loops.
+ * Description: En-tete contenant les prototypes des fonctions
+ * utilisees dans le projet functions_nested_loops.
  */
 
 int _putchar(char c);
 void print_alphabet(void);
+void print_alphabet_x10(void);
 
 #endif /* MAIN_H */
