c1a78bc108ad5a64a8feaea9c6b00d2198442c97
Janice91 <Kouakou.janice.2002@gmail.com>
2025-10-23 11:30:36 +0000

    Fix print_last_digit to handle INT_MIN correctly

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -11,9 +11,14 @@ int print_last_digit(int n)
 	int last_digit;
 
 	if (n < 0)
-		n = -n;
-
-	last_digit = n % 10;
+	{
+		/* gérer le cas spécial INT_MIN */
+		last_digit = (n % 10) * -1;
+	}
+	else
+	{
+		last_digit = n % 10;
+	}
 	_putchar('0' + last_digit);
 	return (last_digit);
 }
