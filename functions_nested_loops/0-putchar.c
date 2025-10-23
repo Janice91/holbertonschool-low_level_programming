b1f1a39da8bf3002105760afa3dcc7c1561197ba
Janice91 <Kouakou.janice.2002@gmail.com>
2025-10-23 10:44:30 +0000

    Ajout ou mise à jour du README.md pour functions_nested_loops

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - point d'entrée du programme
+ *
+ * Description: affiche "_putchar" suivi d’un retour à la ligne
+ * Return: Toujours 0 (succès)
+ */
+int main(void)
+{
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
+    return (0);
+}
