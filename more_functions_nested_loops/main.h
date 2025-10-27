bb26a45bcafe646b22b0bf00905a84e902643def
Janice91 <Kouakou.janice.2002@gmail.com>
2025-10-27 12:43:08 +0000

    Add main.h with all function prototypes

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,11 +1,41 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
-void print_numbers(void);
+/* Prototype de _putchar */
 int _putchar(char c);
+
+/* 0. isupper */
+int _isupper(int c);
+
+/* 1. isdigit */
+int _isdigit(int c);
+
+/* 2. Multiplication */
+int mul(int a, int b);
+
+/* 3. Print numbers 0-9 */
+void print_numbers(void);
+
+/* 4. Print numbers 0-9 except 2 and 4 */
+void print_most_numbers(void);
+
+/* 5. Print numbers 0-14 ten times */
+void more_numbers(void);
+
+/* 6. Draw a straight line */
+void print_line(int n);
+
+/* 7. Draw a diagonal line */
+void print_diagonal(int n);
+
+/* 8. Print a square of # */
+void print_square(int size);
+
+/* 9. Fizz-Buzz program */
+void fizz_buzz(void);
+
+/* 10. Print a triangle */
+void print_triangle(int size);
 
 #endif /* MAIN_H */
 
