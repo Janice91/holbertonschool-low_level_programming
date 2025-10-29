3c19abf52d1e99fd2a964defa98a3f31d93c3d8a
Janice91 <Kouakou.janice.2002@gmail.com>
2025-10-29 21:08:03 +0000

    Add corrected 100-atoi.c in Betty style

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -3,11 +3,13 @@
 /**
  * _atoi - convertit une chaîne en entier
  * @s: chaîne à convertir
+ *
  * Return: entier correspondant à la chaîne
  */
 int _atoi(char *s)
 {
-	int i = 0, sign = 1, result = 0;
+	int i = 0, sign = 1;
+	unsigned int result = 0;
 
 	while (s[i] != '\0')
 	{
@@ -16,13 +18,12 @@ int _atoi(char *s)
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
 			result = result * 10 + (s[i] - '0');
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
 		}
 		else if (result > 0)
 			break;
 		i++;
 	}
 
-	return (sign * result);
+	return ((int)(result * sign));
 }
+
