cb58e190342136a1e7977c4ac4fd8a0b691db501
Janice91 <Kouakou.janice.2002@gmail.com>
2025-10-29 20:53:45 +0000

    Ajout de main.h

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,22 +1,41 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * reset_to_98 - met la valeur pointée à 98
- * @n: pointeur vers un entier
- *
- * Return: rien
- */
+/* Exercice 0 */
 void reset_to_98(int *n);
 
-/**
- * swap_int - échange les valeurs de deux entiers
- * @a: pointeur vers le premier entier
- * @b: pointeur vers le second entier
- *
- * Return: rien
- */
+/* Exercice 1 */
 void swap_int(int *a, int *b);
+
+/* Exercice 2 */
+int _strlen(char *s);
+
+/* Exercice 3 */
+void _puts(char *str);
+
+/* Exercice 4 */
+void print_rev(char *s);
+
+/* Exercice 5 */
+void rev_string(char *s);
+
+/* Exercice 6 */
+void puts2(char *str);
+
+/* Exercice 7 */
+void puts_half(char *str);
+
+/* Exercice 8 */
+void print_array(int *a, int n);
+
+/* Exercice 9 */
+char *_strcpy(char *dest, char *src);
+
+/* Exercice 10 */
+int _atoi(char *s);
+
+/* Fonction utilitaire */
+int _putchar(char c);
 
 #endif /* MAIN_H */
 
