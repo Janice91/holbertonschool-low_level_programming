ad8533e65887e94ffde826cc76c706e0df1eda1d
Janice91 <Kouakou.janice.2002@gmail.com>
2025-10-29 20:37:00 +0000

    Ajout de 1-swap.c et main.h pour l'exercice 1

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,6 +1,22 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * reset_to_98 - met la valeur pointée à 98
+ * @n: pointeur vers un entier
+ *
+ * Return: rien
+ */
 void reset_to_98(int *n);
 
-#endif
+/**
+ * swap_int - échange les valeurs de deux entiers
+ * @a: pointeur vers le premier entier
+ * @b: pointeur vers le second entier
+ *
+ * Return: rien
+ */
+void swap_int(int *a, int *b);
+
+#endif /* MAIN_H */
+
