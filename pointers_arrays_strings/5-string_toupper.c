0fbcc5ff557efc062be8b1f0422884495b9b8f25
Janice91 <Kouakou.janice.2002@gmail.com>
2025-10-30 11:20:26 +0000

    Correction Betty style de 5-string_toupper.c

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,21 +1,23 @@
 #include "main.h"
 
 /**
- * string_toupper - change toutes les lettres minuscules d’une chaîne en majuscules
- * @s: chaîne de caractères
+ * string_toupper - change toutes les lettres minuscules d'une
+ * chaîne en majuscules
+ * @str: chaîne de caractères à modifier
  *
  * Return: pointeur vers la chaîne modifiée
  */
-char *string_toupper(char *s)
+char *string_toupper(char *str)
 {
 	int i = 0;
 
-	while (s[i] != '\0')
+	while (str[i] != '\0')
 	{
-		if (s[i] >= 'a' && s[i] <= 'z')
-			s[i] = s[i] - 32;
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
 		i++;
 	}
 
-	return (s);
+	return (str);
 }
+
