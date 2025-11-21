fd436fcd300f4df82322066674535c3daf882782
Janice91 <Kouakou.janice.2002@gmail.com>
2025-11-21 19:41:32 +0000

    Push current function_pointers files

---
+++ function_pointers/3-main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - Entry point of the program
+ * @argc: Number of arguments
+ * @argv: Array of arguments
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	int (*op_func)(int, int);
+	int a, b;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	op_func = get_op_func(argv[2]);
+	if (!op_func)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", op_func(a, b));
+	return (0);
+}
