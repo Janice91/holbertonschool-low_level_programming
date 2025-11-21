674853462f76c28e94b4a23f1361a51f16470c0d
Janice91 <Kouakou.janice.2002@gmail.com>
2025-11-21 20:45:58 +0000

    Add first three function pointer exercises with main files

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,14 +1,15 @@
 #include "function_pointers.h"
+#include <stddef.h>
 
 /**
- * print_name - prints a name using a given function
- * @name: name of the person
- * @f: pointer to a function that takes a char * and returns void
+ * print_name - prints a name using a function pointer
+ * @name: name to print
+ * @f: function pointer that prints the name
  */
 void print_name(char *name, void (*f)(char *))
 {
-    if (name == NULL || f == NULL)
-        return;
+	if (name == NULL || f == NULL)
+		return;
 
-    f(name);
+	f(name);
 }
