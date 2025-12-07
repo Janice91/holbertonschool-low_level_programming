009f2b3f34ddc72dae4641068ab7cab61f6909ac
Janice91 <Kouakou.janice.2002@gmail.com>
2025-12-07 20:57:51 +0000

    Add append_text_to_file function and test main for exercise 2

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -1,40 +1,40 @@
+#include "main.h"
 #include <fcntl.h>
 #include <unistd.h>
-#include "main.h"
 
 /**
- * append_text_to_file - append text at the end of a file
+ * append_text_to_file - appends text at the end of a file
  * @filename: name of the file
- * @text_content: text to append
+ * @text_content: NULL-terminated string to add
  * Return: 1 on success, -1 on failure
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int fd, len, w;
+    int fd;
+    int len = 0;
+    int w;
 
     if (!filename)
         return (-1);
 
-    /* ouvrir le fichier uniquement en écriture et append */
+    /* ouvrir le fichier seulement s'il existe */
     fd = open(filename, O_WRONLY | O_APPEND);
     if (fd == -1)
-        return (-1); /* fichier n'existe pas ou pas de permission */
+        return (-1);
 
-    if (!text_content)
+    if (!text_content) /* rien à écrire */
     {
         close(fd);
-        return (1); /* rien à ajouter mais fichier existe */
+        return (1);
     }
 
-    /* Calculer la longueur de text_content */
-    for (len = 0; text_content[len]; len++)
-        ;
+    while (text_content[len])
+        len++;
 
-    /* Écrire le contenu */
     w = write(fd, text_content, len);
     close(fd);
 
-    if (w != len)
+    if (w == -1 || w != len)
         return (-1);
 
     return (1);
