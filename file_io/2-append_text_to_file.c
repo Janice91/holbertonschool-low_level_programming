e305e3880d9cc35bd8f70d48a8d6f4636fb663ec
Janice91 <Kouakou.janice.2002@gmail.com>
2025-12-07 20:35:35 +0000

    File IO: add append_text_to_file function and test main for exercise 2

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -3,16 +3,17 @@
 #include <unistd.h>
 
 /**
- * append_text_to_file - append text at the end of file
- * @filename: filename
- * @text_content: content
+ * append_text_to_file - append text at the end of a file
+ * @filename: name of the file
+ * @text_content: string to append
+ *
  * Return: 1 on success, -1 on failure
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
     int fd;
     int i;
-    int w;
+    ssize_t w;
 
     if (!filename)
         return (-1);
@@ -29,15 +30,18 @@ int append_text_to_file(const char *filename, char *text_content)
 
     /* Calculer la longueur du texte */
     i = 0;
-    while (text_content[i])
+    while (text_content[i] != '\0')
         i++;
 
+    /* Écrire le texte à la fin du fichier */
     w = write(fd, text_content, i);
-    close(fd);
-
     if (w == -1 || w != i)
+    {
+        close(fd);  /* Toujours fermer le fichier même en cas d'erreur */
         return (-1);
+    }
 
+    close(fd);
     return (1);
 }
 
