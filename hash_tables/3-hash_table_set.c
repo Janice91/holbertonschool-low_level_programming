df32eff0085cbbbd32fe75cdba87bbed5e64dfdf
Janice91 <Kouakou.janice.2002@gmail.com>
2025-12-07 00:12:03 +0000

    Ajout des fichiers pour hash_tables

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,79 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * create_node - creates a new hash node
+ * @key: the key
+ * @value: the value
+ *
+ * Return: pointer to the new node or NULL if failed
+ */
+hash_node_t *create_node(const char *key, const char *value)
+{
+	hash_node_t *node;
+
+	node = malloc(sizeof(hash_node_t));
+	if (!node)
+		return (NULL);
+
+	node->key = strdup(key);
+	if (!node->key)
+	{
+		free(node);
+		return (NULL);
+	}
+
+	node->value = strdup(value);
+	if (!node->value)
+	{
+		free(node->key);
+		free(node);
+		return (NULL);
+	}
+
+	node->next = NULL;
+	return (node);
+}
+
+/**
+ * hash_table_set - adds or updates an element in the hash table
+ * @ht: hash table
+ * @key: key (cannot be empty)
+ * @value: value associated with key
+ *
+ * Return: 1 if success, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *tmp, *new_node;
+	unsigned long int index;
+
+	if (!ht || !key || *key == '\0' || !value)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[index];
+
+	while (tmp)
+	{
+		if (strcmp(tmp->key, key) == 0)
+		{
+			free(tmp->value);
+			tmp->value = strdup(value);
+			if (!tmp->value)
+				return (0);
+			return (1);
+		}
+		tmp = tmp->next;
+	}
+
+	new_node = create_node(key, value);
+	if (!new_node)
+		return (0);
+
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+
+	return (1);
+}
